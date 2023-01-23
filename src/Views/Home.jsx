449ef4fb6ab0b8f415eec9f4e071c345cee589c7
Gokenve <gokenve@gmail.com>
2023-01-23 20:10:51 +0100

    feat_get_data_api_&_componets_allmovies_allcinemas

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,1 +1,17 @@
+import "../styles/Home.scss";
 import React from "react";
+import AllMovies from "./AllMovies";
+import AllCinemas from "./AllCinemas";
+
+function Home ({ listMovies, listCinemas }) {
+    //console.log(`Home page : ${ReqsApi}`);
+    return (
+        <div className="Home">
+            <AllMovies listMovies={listMovies} />
+            <AllCinemas listCinemas = {listCinemas} />
+        </div>
+    )
+ 
+}
+
+export default Home;
